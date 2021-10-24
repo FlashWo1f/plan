d34bb9ad4eb6012a5af6903ffe9fb8350e529f9e
Libin <1064048947@qq.com>
2021-10-24 14:04:09 +0800

    Promise.then .all

--- 手写代码/Promise.js
+++ 手写代码/Promise.js
@@ -80,15 +80,78 @@ class LPromise {
       resolve(result)
     }
   }
+
+  catch = (onRejected) => this.then(null, onRejected) 
+
+  static all = (tasks) => {
+    if (!Array.isArray(tasks)) return
+    return new LPromise((resolve, reject) => {
+      let result = [], count = 0
+      tasks.forEach((item, idx) => {
+        item.then(
+          res => {
+            // 保证结果的顺序
+            result[idx] = res
+            if (++count === tasks.length) resolve(result)
+          },
+          reject
+        )
+      })
+    })
+  }
+
+  static race = (tasks) => {
+    if (!Array.isArray(tasks)) return
+    return new LPromise((resolve, reject) => {
+      tasks.forEach(item => {
+        if (item && isFn(item.then)) {
+          item.then(res => {
+            resolve(res)
+          }, reject)
+        }
+      })
+    })
+  }
 }
 
-// example 
 const { log } = console
-const p1 = new LPromise((resolve, reject) => {
-  resolve(2)
-  log(1)
-}).then(res => {
-  log(res)
-  return 3
-}).then(res => log(res))
-log(0)
+// example 1 start
+// const p1 = new LPromise((resolve, reject) => {
+//   resolve(2)
+//   log(1)
+// }).then(res => {
+//   log(res)
+//   return 3
+// }).then(res => log(res))
+// log(0)
+// example 1 end
+
+// example 2 start
+const pp1 = new LPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('all1')
+  }, 300)
+})
+const pp2 = new LPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('all2')
+  }, 100)
+})
+const pp3 = new LPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('all3')
+  }, 50)
+})
+const pp4 = new LPromise((resolve, reject) => {
+  setTimeout(() => {
+    reject('all4')
+  }, 200)
+})
+// const all1 = LPromise.all([pp1, pp2, pp3]).then(res => {
+//   console.log('all fulfilled', res)
+// })
+const all2 = LPromise.all([pp4, pp2, pp3]).then(res => {
+}, err => console.log('someone rejected', err))
+// example 2 end
+
+
